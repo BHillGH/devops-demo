e803779aa927d098bdf8ee96345b0d9f0291df4a
Brandon <hello@brandonhill.au>
2024-11-01 12:35:54 +0800

    Moved to simple echo app

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ FROM golang:1.23-alpine AS build-stage
 WORKDIR /build
 
 COPY go.mod ./
+COPY go.sum ./
 RUN go mod download
 
 COPY *.go ./
-COPY /data/ ./data/
 
 RUN CGO_ENABLED=0 GOOS=linux go build -o ./devops-demo
 
@@ -19,6 +19,5 @@ RUN apk update
 WORKDIR /app
 
 COPY --from=build-stage /build/devops-demo .
-COPY --from=build-stage /build/data ./data
 
 ENTRYPOINT ["./devops-demo"]
--- main.go
+++ main.go
@@ -1,35 +1,15 @@
 package main
 
 import (
-	"flag"
-	"log"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
-
-func serveSocket() http.Handler {
-	hub := newHub()
-	go hub.run()
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)
-	})
-
-}
-
 func main() {
-	flag.Parse()
-
-	dir := http.Dir("./frontend")
-	fs := http.FileServer(dir)
-
-	mux := http.NewServeMux()
-
-	mux.Handle("/", fs)
-	mux.Handle("/ws", serveSocket())
-
-	err := http.ListenAndServe(*addr, mux)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
